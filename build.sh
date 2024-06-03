9f3531a95323d3a17e2ce9e0e2e0fad26fb847ec
Anduin <anduin@aiursoft.com>
2024-06-03 12:21:36 +0000

    chore: Update Dockerfile to use GitHub Container Registry for image build and push

--- build.sh
+++ build.sh
@@ -6,14 +6,11 @@ set -e
 tput setaf 2; echo "Cleaning up..."; tput sgr0
 rm ./node_modules -rf
 
-tput setaf 2; echo "Installing markdown lint"; tput sgr0
-gem install mdl || sudo gem install mdl
-
 tput setaf 2; echo "Generating new readme and mkdocs"; tput sgr0
 node ./.github/readme-generate.js
 
 tput setaf 2; echo "Running markdown lint to check issues."; tput sgr0
-mdl ./dishes ./tips -r ~MD036,~MD024,~MD004,~MD029,~MD013,~MD007
+markdownlint ./dishes ./tips
 
 tput setaf 2; echo "Installing python requirements..."; tput sgr0
 pip install -r requirements.txt --break-system-packages
