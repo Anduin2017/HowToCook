9c81c6037eae331aa911a478926a97360a7e3ae7
justbeay <lizjut@gmail.com>
2025-08-16 19:22:36 +0800

    update script: build-with-docker.sh

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+node_image=node:22-alpine
+python_image=python:3.11
+
 run_in_docker() {
     image="$1"
     command="$2"
@@ -12,16 +16,27 @@ run_in_docker() {
 }
 
 
-node_image=node:22-alpine
-python_image=python:3.11
+# npm builld
+run_in_docker $node_image "
+npm install --loglevel verbose;
+npm run build;
+npm run lint;
+"
 
-run_in_docker $node_image "npm install --loglevel verbose"
-run_in_docker $node_image "npm run build"
-run_in_docker $node_image "npm run lint"
+# mkdocs build
+echo """
+FROM $python_image
 
-run_in_docker $python_image "apt-get update && apt-get install -y weasyprint fonts-noto-cjk wget unzip"
-run_in_docker $python_image "rm node_modules -rf && pip install -r requirements.txt"
-run_in_docker $python_image "pip install mkdocs && mkdocs build"
+RUN apt-get update && apt-get install -y weasyprint fonts-noto-cjk wget unzip
+RUN pip install mkdocs
+COPY requirements.txt ./
+RUN pip install -r requirements.txt
+
+""" >/tmp/Dockerfile.mkdocs
+
+docker build . -f /tmp/Dockerfile.mkdocs -t mkdocs-env
+rm -rf site node_modules
+run_in_docker mkdocs-env "mkdocs build"
 
 # Tips: when we clone the remote repo to local, we would found the image files are unabe to open and the contents are like such:
 #   version https://git-lfs.github.com/spec/v1
